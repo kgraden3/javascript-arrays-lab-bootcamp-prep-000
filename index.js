513f35a0f5f6aae97d5156fdf194f0372e91a00f
Kyle Graden <kylegraden3@gmail.com>
2019-02-12 02:18:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,34 @@
-var kittens = [] //define your array here
-
+var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 // Add your functions and code here
+function destructivelyAppendKitten(name){
+  return kittens.push("John")
+}
+
+function destructivelyPrependKitten(name){
+  return kittens.unshift("John")
+}
+
+function destructivelyRemoveLastKitten(){
+  return kittens.pop()
+}
+
+
+function destructivelyRemoveFirstKitten(){
+  return kittens.shift()
+}
+
+function appendKitten(name){
+  return [...kittens, "John",]
+}
+
+function prependKitten(name){
+  return ["John", ...kittens]
+}
+
+function removeLastKitten(){
+  return kittens.slice(0, kittens.length - 1)
+}
+
+function removeFirstKitten(){
+  return kittens.slice(1)
+}
